0835495d3e485b148e8af46621a965629680fa9b
Joaquin Quintas <joako84@gmail.com>
2018-05-08 11:02:08 -0300

    import db

--- seguros/tickets/management/commands/import_infoauto_db.py
+++ seguros/tickets/management/commands/import_infoauto_db.py
@@ -20,8 +20,8 @@ ROOT_DIR = environ.Path(__file__) - 4
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.import_brands()
-        self.import_models()
+        #self.import_brands()
+        #self.import_models()
         self.import_versions()
     
     def import_brands(self):
@@ -194,4 +194,112 @@ class Command(BaseCommand):
                                                   model=model,
                                                   year=2008,
                                                   price=price_2008 * 1000)
+                 
+                price_2007 = int(row[18])
+                if price_2007 != 0:
+                    try:
+                        CarVersion.objects.get(slug=slug, year=2007)
+                    except CarVersion.DoesNotExist:
+                        CarVersion.objects.create(infoauto_id=infoauto_id, 
+                                                  name=name, slug=slug,
+                                                  model=model,
+                                                  year=2007,
+                                                  price=price_2007 * 1000)
+                price_2006 = int(row[19])
+                if price_2006 != 0:
+                    try:
+                        CarVersion.objects.get(slug=slug, year=2006)
+                    except CarVersion.DoesNotExist:
+                        CarVersion.objects.create(infoauto_id=infoauto_id, 
+                                                  name=name, slug=slug,
+                                                  model=model,
+                                                  year=2006,
+                                                  price=price_2006 * 1000) 
+                price_2005 = int(row[20])
+                if price_2005 != 0:
+                    try:
+                        CarVersion.objects.get(slug=slug, year=2005)
+                    except CarVersion.DoesNotExist:
+                        CarVersion.objects.create(infoauto_id=infoauto_id, 
+                                                  name=name, slug=slug,
+                                                  model=model,
+                                                  year=2005,
+                                                  price=price_2005 * 1000) 
+                
+                
+                price_2004 = int(row[21])
+                if price_2004 != 0:
+                    try:
+                        CarVersion.objects.get(slug=slug, year=2004)
+                    except CarVersion.DoesNotExist:
+                        CarVersion.objects.create(infoauto_id=infoauto_id, 
+                                                  name=name, slug=slug,
+                                                  model=model,
+                                                  year=2004,
+                                                  price=price_2004 * 1000) 
                         
+                price_2003 = int(row[22])
+                if price_2003 != 0:
+                    try:
+                        CarVersion.objects.get(slug=slug, year=2003)
+                    except CarVersion.DoesNotExist:
+                        CarVersion.objects.create(infoauto_id=infoauto_id, 
+                                                  name=name, slug=slug,
+                                                  model=model,
+                                                  year=2003,
+                                                  price=price_2003 * 1000)
+                
+                price_2002 = int(row[23])
+                if price_2002 != 0:
+                    try:
+                        CarVersion.objects.get(slug=slug, year=2002)
+                    except CarVersion.DoesNotExist:
+                        CarVersion.objects.create(infoauto_id=infoauto_id, 
+                                                  name=name, slug=slug,
+                                                  model=model,
+                                                  year=2002,
+                                                  price=price_2002 * 1000)
+                        
+                price_2001 = int(row[24])
+                if price_2001 != 0:
+                    try:
+                        CarVersion.objects.get(slug=slug, year=2001)
+                    except CarVersion.DoesNotExist:
+                        CarVersion.objects.create(infoauto_id=infoauto_id, 
+                                                  name=name, slug=slug,
+                                                  model=model,
+                                                  year=2001,
+                                                  price=price_2001 * 1000)
+                        
+                price_2000 = int(row[25])
+                if price_2000 != 0:
+                    try:
+                        CarVersion.objects.get(slug=slug, year=2000)
+                    except CarVersion.DoesNotExist:
+                        CarVersion.objects.create(infoauto_id=infoauto_id, 
+                                                  name=name, slug=slug,
+                                                  model=model,
+                                                  year=2000,
+                                                  price=price_2000 * 1000)
+                
+                price_1999 = int(row[26])
+                if price_1999 != 0:
+                    try:
+                        CarVersion.objects.get(slug=slug, year=1999)
+                    except CarVersion.DoesNotExist:
+                        CarVersion.objects.create(infoauto_id=infoauto_id, 
+                                                  name=name, slug=slug,
+                                                  model=model,
+                                                  year=1999,
+                                                  price=price_1999 * 1000)
+                
+                price_1998 = int(row[27])
+                if price_1998 != 0:
+                    try:
+                        CarVersion.objects.get(slug=slug, year=1998)
+                    except CarVersion.DoesNotExist:
+                        CarVersion.objects.create(infoauto_id=infoauto_id, 
+                                                  name=name, slug=slug,
+                                                  model=model,
+                                                  year=1998,
+                                                  price=price_1998 * 1000)
